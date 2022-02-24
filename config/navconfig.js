48e36bc5257a4b15989fc5c4f8945508eaa9c2c3
xiaohuamao <maoxh@jiguang.cn>
2022-02-24 18:34:40 +0800

    feat: 增加webpack和browser

--- config/navconfig.js
+++ config/navconfig.js
@@ -1,28 +1,40 @@
 module.exports = [
-    { text: '首页', link: '/' },
-    { text: '笔记',  items: [
-            { text: '日常记录', link: '/guide/day/' },
-            { text: '面试题', link: '/guide/audition/' },
-        ]
-    },
-    {
-        text: '前端', items: [
-            { text: 'Vue', link: '/front/vue/' },
-            { text: 'React', link: '/front/react/' },
-            { text: 'Angular', link: '/front/angular/' },
-            { text: 'css', link: '/front/css/' },
-        ]
-    },
-    {
-        text: 'node', link: '/node/'},
-    {
-        text: '服务器部署', link: '/server/'},
-    {
-        text: 'git', link: '/git/'},
-    {
-        text: '其它', items: [
-            { text: 'CICD', link: '/others/cicd/'},
-            { text: 'Issues', link: '/others/issues/' },
-        ]
-    }
-]
+  { text: "首页", link: "/" },
+  {
+    text: "笔记",
+    items: [
+      { text: "日常记录", link: "/guide/day/" },
+      { text: "面试题", link: "/guide/audition/" },
+      { text: "浏览器", link: "/guide/browser/" },
+      { text: "webpack", link: "/guide/webpack/" },
+    ],
+  },
+  {
+    text: "前端",
+    items: [
+      { text: "Vue", link: "/front/vue/" },
+      { text: "React", link: "/front/react/" },
+      { text: "Angular", link: "/front/angular/" },
+      { text: "css", link: "/front/css/" },
+    ],
+  },
+  {
+    text: "node",
+    link: "/node/",
+  },
+  {
+    text: "服务器部署",
+    link: "/server/",
+  },
+  {
+    text: "git",
+    link: "/git/",
+  },
+  {
+    text: "其它",
+    items: [
+      { text: "CICD", link: "/others/cicd/" },
+      { text: "Issues", link: "/others/issues/" },
+    ],
+  },
+];
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,41 +1,28 @@
-const head = require('../../config/headconfig.js')
-const plugins = require('../../config/pluginconfig.js')
-const nav = require('../../config/navconfig.js')
+const head = require("../../config/headconfig.js");
+const plugins = require("../../config/pluginconfig.js");
+const nav = require("../../config/navconfig.js");
 
 module.exports = {
-    title: 'xiaohuamao',
-    description: 'Just playing around',
-    head: head,
-    plugins: plugins,
-    base: '/',
-    themeConfig: { //主题配置
-        repo: 'xiaohuamao93/myBlog',
-        docsDir: 'docs',
-        editLinks: true,
-        sidebar: {
-            '/guide/day/': [
-                '',
-            ],
-            '/guide/audition/': [
-                '',
-                'xiaoying',
-                'nodejs'
-            ],
-            '/server/': [
-                '',
-                'nginx',
-                'jenkins',
-                'tomcat',
-                'vim',
-                'pm2',
-                'ip'
-            ],
-            '/git/': [
-                ''
-            ]
-        },
-        // editLinkText: '编辑！',
-        lastUpdated: '上次更新', // string | boolean
-        nav: nav
-    }
-}
+  title: "xiaohuamao",
+  description: "Just playing around",
+  head: head,
+  plugins: plugins,
+  base: "/",
+  themeConfig: {
+    //主题配置
+    repo: "xiaohuamao93/myBlog",
+    docsDir: "docs",
+    editLinks: true,
+    sidebar: {
+      "/guide/day/": [""],
+      "/guide/audition/": ["", "xiaoying", "nodejs"],
+      "/guide/browser/": [""],
+      "/guide/webpack/": [""],
+      "/server/": ["", "nginx", "jenkins", "tomcat", "vim", "pm2", "ip"],
+      "/git/": [""],
+    },
+    // editLinkText: '编辑！',
+    lastUpdated: "上次更新", // string | boolean
+    nav: nav,
+  },
+};
